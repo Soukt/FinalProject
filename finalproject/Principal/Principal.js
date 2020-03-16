4dc5ec1cd408a7f32eaf7011ddd95c5bb97bc944
Martin <mtorresfelgueroso@gmail.com>
2020-03-16 12:12:42 +0100

    push inicial

---
+++ finalproject/Principal/Principal.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Principal.css';
+
+class Principal extends Component {
+  // constructor(props){
+    // super(props);
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Principal;
